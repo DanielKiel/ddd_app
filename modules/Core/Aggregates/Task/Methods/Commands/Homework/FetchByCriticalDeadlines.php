dfd298e20790eb5e438150c7926d9092339ff5ad
Daniel Koch <daniel.koch@publicare.de>
2018-01-27 10:02:37 +0100

    fetching by time range

--- modules/Core/Aggregates/Task/Methods/Commands/Homework/FetchByCriticalDeadlines.php
+++ modules/Core/Aggregates/Task/Methods/Commands/Homework/FetchByCriticalDeadlines.php
@@ -17,6 +17,8 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 class FetchByCriticalDeadlines
 {
     /**
+     * write something like addDays(2) to get all tasks which have homework where the deadline is before now + 2 days
+     * it will be about the user configs what will be the expression - not the decision of the method what will be critical
      * @param $expression
      * @return Collection
      */
@@ -30,6 +32,30 @@ class FetchByCriticalDeadlines
         ]);
 
         $result = Homework::where('deadline', '<', $critical)
+            ->where('status', '!=', 'closed')
+            ->select('task_id')
+            ->groupBy('task_id')
+            ->get();
+
+        $return = collect();
+        $repo = app()->make('Core_Task_DBRepo');
+
+        $result->each(function($homework) use($return, $repo) {
+            $return->push($repo->findById($homework->task_id));
+        });
+
+        return $return;
+    }
+
+    /**
+     * @param Carbon $from
+     * @param Carbon $to
+     * @return Collection
+     */
+    public function fetchByTimeRange(Carbon $from, Carbon $to): Collection
+    {
+        $result = Homework::where('deadline', '>=', $from)
+            ->where('deadline', '<=', $to)
             ->where('status', '!=', 'closed')
             ->select('task_id')
             ->groupBy('task_id')
--- tests/Feature/Core/Task/HomeworkGetTest.php
+++ tests/Feature/Core/Task/HomeworkGetTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 
 class HomeworkGetTest extends TestCase
 {
-    public function testGetCriticalHomeworks()
+    public function testGetCriticalHomework()
     {
         $student = $this->app->make('Core_Student_DBRepo')->setStruct([])->commit();
         $subject = $this->app->make('Core_Subject_DBRepo')->setStruct(['name' => '5a'])->commit();
@@ -70,6 +70,77 @@ class HomeworkGetTest extends TestCase
         $method = app()->make('Core_Task_Methods_FetchByCriticalDeadline');
 
         $result = $method->fetchByTimeExpression('addDay(-2)');
+
+        $this->assertEquals(2, $result->count());
+    }
+
+    public function testGetHomeworkByTimerange()
+    {
+        $student = $this->app->make('Core_Student_DBRepo')->setStruct([])->commit();
+        $subject = $this->app->make('Core_Subject_DBRepo')->setStruct(['name' => '5a'])->commit();
+
+        $method = app()->make('Core_Task_Methods_CreatingHomework');
+
+        $critical = Carbon::now()->addDay(-5);
+        $method->createAHomeworkSeriesByStudent($student->id, $subject->id, [
+            [
+                'deadline' => $critical,
+                'todo' => 'Critical 1',
+            ],
+            [
+                'deadline' => $critical,
+                'todo' => 'Critical 2',
+            ],
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 1',
+            ],
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 2',
+            ]
+        ]);
+
+        $method->createAHomeworkSeriesByStudent($student->id, $subject->id, [
+            [
+                'deadline' => $critical,
+                'todo' => 'B-Critical 1',
+            ],
+            [
+                'deadline' => $critical,
+                'todo' => 'B-Critical 2',
+            ],
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 1',
+            ],
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 2',
+            ]
+        ]);
+
+        //here no critical times will be
+        $method->createAHomeworkSeriesByStudent($student->id, $subject->id, [
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 1',
+            ],
+            [
+                'deadline' => Carbon::now()->addDay(5),
+                'todo' => 'Was 2',
+            ]
+        ]);
+
+        /** @var FetchByCriticalDeadlines $method */
+        $method = app()->make('Core_Task_Methods_FetchByCriticalDeadline');
+
+        $result = $method->fetchByTimeRange(Carbon::now(), Carbon::now()->addDay(5));
+
+        $this->assertEquals(3, $result->count());
+
+
+        $result = $method->fetchByTimeRange(Carbon::now()->addDay(-5), Carbon::now());
 
         $this->assertEquals(2, $result->count());
     }
